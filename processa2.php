8b8ccd4f7e85b36d77c97d3833a23d26acedddd7
marcelommello <44443212+marcelommello@users.noreply.github.com>
2018-10-31 15:19:06 -0300

    Add files via upload
    Exibir Musica

---
+++ processa2.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once("conexao2.php");
+
+$consulta = "SELECT * FROM musicas";
+$con = mysqli-query($consulta) or die($mysqli-error);
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf8">
+		<title>Musicas</title>
+	</head>
+	<body>
+		<table>
+			<tr>
+				<td>Nome</td>
+				<td>Artista</td>
+				<td>Duração</td>
+				<td>Album</td>
+				<td>Genêro</td>
+				<td>Status</td>
+				<td>ID da Música</td>
+			</tr>
+			<?php while($dado = $con->fecth_array()){?>
+			<tr>
+				<td><?php echo $dado ["Nome"];?></td>
+				<td><?php echo $dado ["Artista"];?></td>
+				<td><?php echo $dado ["Duracao"];?></td>
+				<td><?php echo $dado ["Album"];?></td>
+				<td><?php echo $dado ["Genero"];?></td>
+				<td><?php echo $dado ["status"];?></td>
+				<td><?php echo $dado ["ID_M"];?></td>
+			</tr>
+			<?php } ?>
+		</table>
+	</body>
+</html>
