494a864b7aace457cc6c48f802ebdea176b2ebfd
Goncalves-Rafael <42102420+Goncalves-Rafael@users.noreply.github.com>
2018-11-04 17:35:08 -0200

    Adicionado deletar música da playlist
    Adicionada função que deletar música da playlist usando o campo action com valor "deletarMusicaPlaylist" e com id da música e da playlist como GET.

--- clientHandler.php
+++ clientHandler.php
@@ -17,13 +17,15 @@
                 echo "<script>alert('Falha ao adicionar.')</script>";
             }
             break;
-        case "selecionarMusicasPlaylists":
+        case "selecionarPlaylistsParaMusica":
             $query = "SELECT * FROM playlist WHERE status = 1";
             $playlists = $conexao->query($query);
-            $id_m = $_GET["id_M"];
-            $query = "SELECT * FROM musicas WHERE ID_M = ".$id_m.";";
-            $verifica = $conexao->query($query);
-            $musica = $verifica->fetch_assoc();
+            if(isset($_GET["id_m"])){
+                $id_m = $_GET["id_m"];
+                $query = "SELECT * FROM musicas WHERE ID_M = ".$id_m.";";
+                $verifica = $conexao->query($query);
+                $musica = $verifica->fetch_assoc();
+            }
             break;
         case "deletarMusica":
             $id = intval($_GET["id"]);
@@ -39,7 +41,7 @@
 
             if(!$verifica){
                 echo "<script>alert(\"Erro de conexão.\")</script>";
-                echo "<meta http-equiv=\"refresh\" content=\"0; url=adicionarMusicaPlaylist.php?action=selecionarMusicasPlaylists&id_M=".$id_M."\">";
+                echo "<meta http-equiv=\"refresh\" content=\"0; url=adicionarMusicaPlaylist.php?action=selecionarPlaylistsParaMusica&id_M=".$id_M."\">";
                 break;
             }
 
@@ -66,6 +68,14 @@
             $verifica = $conexao->query($query);
             $playlist = $verifica->fetch_assoc();
             break;
+        case "exibirPlaylist":
+            $id_P = intval($_GET["id_p"]);
+            $query = "SELECT * FROM playlist WHERE ID_P = ".$id_P.";";
+            $verifica = $conexao->query($query);
+            $playlist = $verifica->fetch_assoc();
+            $query = "SELECT pertence.status, pertence.ID_pertence, pertence.ID_M, musicas.ID_M, musicas.Nome FROM pertence JOIN musicas ON musicas.ID_M=pertence.ID_M WHERE ID_P = ".$id_P." && pertence.status = 1;";
+            $musicasPlaylist = $conexao->query($query);
+            break;
         case "salvarPlaylist":
             $id_P = intval($_POST["id_p"]);
             $query = "UPDATE `playlist` SET `Nome`=\"".$_POST["nome"]."\",`Descricao`=\"".$_POST["descricao"]."\" WHERE ID_P = \"".$id_P."\";";
@@ -75,6 +85,19 @@
                 echo "<meta http-equiv=\"refresh\" content=\"0; url=exibirplaylists.php\">";
             }else{
                 echo "<script>alert(\"Erro ao salvar alterações.\")</script>";
+                echo "<meta http-equiv=\"refresh\" content=\"0; url=editarPlaylist.php?action=editarPlaylist&id_p=".$id_P."\">";
+            }
+            break;
+        case "deletarMusicaPlaylist":
+            $id_P = intval($_GET["id_p"]);
+            $id_M = intval($_GET["id_m"]);
+            $query = "UPDATE pertence SET status = 0 WHERE ID_P = $id_P && ID_M = $id_M";
+            $verifica = $conexao->query($query);
+            if($verifica){
+                echo "<script>alert(\"Música deletada da playlist com sucesso.\")</script>";
+                echo "<meta http-equiv=\"refresh\" content=\"0; url=exibirplaylists.php\">";
+            }else{
+                echo "<script>alert(\"Erro ao deletar música da playlist.\")</script>";
                 echo "<meta http-equiv=\"refresh\" content=\"0; url=editarPlaylist.php?action=editarPlaylist&id_p=".$id_P."\">";
             }
             break;
