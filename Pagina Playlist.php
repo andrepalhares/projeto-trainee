911e8fd2a9b3f5d2e95f3a1182684d5bec53af41
marcelommello <44443212+marcelommello@users.noreply.github.com>
2018-11-06 22:16:33 -0200

    Update Pagina Playlist.php

--- Pagina Playlist.php
+++ Pagina Playlist.php
@@ -4,26 +4,30 @@
 
 <head>
 	<title>Adicionar Playlist</title>
+	<meta charset="utf8">
 </head>
 <body>
 	<header>
-		<h1> Adicionar Playlist</h1>
 		<nav class="menu">
 			<ul>
-				<li><a href="Inicial.html">Pagina Inicial</a></li>
+				<li><a href="Inicial.html">Inicio</a></li>
+				<li><a href="exibirPlaylist.html">Adicionar Musica</a></li>
+				<li><a href="exibirPlaylist.html">Adicionar Playlist</a></li>
+				<li><a href="Exibir_musicas.php">Listar Musica</a></li>
 			</ul>
 		</nav>
 	</header>
 <div class="formulario">
 		<form action="processa.php" method="post">
 			<br>
-			<fieldset>
-				<legend>Nova Playlist</legend>
+			
+				<h1>Nova Playlist</h1>
 				<br>
-			<input type="text" placeholder="Nome" name="Nome"/><br><br>
-			<textarea placeholder="Descricao" rows="4" cols="100" name="Descricao"></textarea><br><br>
+			Nome:<br><input type="text" placeholder="Nome" name="Nome"/><br><br>
+			Descrição:
+			<br><textarea placeholder="Descrição" rows="4" cols="100" name="Descricao"></textarea><br><br>
 			<input type="submit" value="Adicionar" class="btn">
-			</fieldset>
+			
 		</form>
 	</div>
 </body>
