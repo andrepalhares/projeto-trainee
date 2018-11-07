e17549ea66261a0d23b4a72a22639d655beb1344
marcelommello <44443212+marcelommello@users.noreply.github.com>
2018-11-07 15:33:06 -0200

    Update Pagina Playlist.php

--- Pagina Playlist.php
+++ Pagina Playlist.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="estilo1.css"/>
 	<title>Adicionar Playlist</title>
 	<meta charset="utf8">
